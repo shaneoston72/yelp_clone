ea23d1c851f59602d0472e8d73684db5fc5824c5
Shane Oston Stowe <shane@shaneoston.com>
2016-04-13 18:07:24 +0100

    facebook integration

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -4,6 +4,7 @@ feature 'User can sign in and out' do
   context 'user not signed in and on the homepage' do
     it "should see a 'sign in' link and a 'sign up' link" do
       visit '/'
+      expect(page).to have_link('Sign in with Facebook')
       expect(page).to have_link('Sign in')
       expect(page).to have_link('Sign up')
     end
