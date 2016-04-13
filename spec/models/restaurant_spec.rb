9deaf1f7573eb8ef8a35b9d856b0f9bd48bcecac
Shane Oston Stowe <shane@shaneoston.com>
2016-04-13 13:40:57 +0100

    added restaurant delete logic and minimum length on restaurant name

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews}
+
+  it 'deletes associated reviews when restaurant is deleted' do
+    should have_many(:reviews).dependent(:destroy)
+  end
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: 'kf')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
 end
